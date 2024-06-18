a8f6e2a3a1c512813896314bfe002a803dacb794
lorenzo-buffo <lolitobuffo@gmail.com>
2024-06-18 15:23:32 -0300

    dibujo de boton play

--- scenes/Start.js
+++ scenes/Start.js
@@ -10,10 +10,10 @@ this.load.image("Play", "../public/assets/play.png")
 
 create(){
     this.add.text(400, 300, 'TANGO FERRUM 2178', { fontSize: '48px', fill: '#fff' }).setOrigin(0.5);
-    
-    const playButton = this.add.image(400, 450, 'Play').setOrigin(0.5);
+
+    const playButton = this.add.image(100, 450, 'Play').setOrigin(0.5);
     playButton.setInteractive({ cursor: 'pointer' });
-    playButton.setScale(0.2); 
+    playButton.setScale(0.4); 
 
     playButton.on('pointerdown', () => {
         // Reiniciar el juego, en este caso, regresamos a la escena principal
